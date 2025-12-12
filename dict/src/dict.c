df9bb49d6b3431b9a85eb4339be3a43d203addaa
Eliomar Rodriguez <104672885+eliorodr2104@users.noreply.github.com>
2025-12-12 18:31:53 +0100

    Refactor memory management and implement dict_get logic
    Replaced malloc/free with zmalloc for dictionary allocation and initialization. Implemented retain_dict and release_dict for reference counting and safe memory release. Added initial logic for dict_get to search in the rehashing array.

--- dict/src/dict.c
+++ dict/src/dict.c
@@ -13,15 +13,10 @@ init_dict(
     size_t (*hash)   (let_any key),
     int    (*compare)(let_any a, let_any b)
 ) {
-    Dictionary *dict = malloc(sizeof(*dict));
-    if (!dict) return NULL;
+    Dictionary *dict = zmalloc(sizeof(*dict));
 
     // Init Arrays on dictionary
-    dict->array[0].elements = malloc(MIN_SIZE * sizeof(dict->array[0]));
-    if (!dict->array[0].elements) {
-        free(dict);
-        return NULL;
-    }
+    dict->array[0].elements     = zmalloc(MIN_SIZE * sizeof(dict->array[0]));
     dict->array[0].size         = MIN_SIZE;
     dict->array[0].num_elements = 0;
     dict->array[1].elements     = NULL;
@@ -39,16 +34,58 @@ init_dict(
 }
 
 Dictionary*
-retain_dict(Dictionary* self);
+retain_dict(Dictionary* self) {
+    if (!self) return NULL;
+
+    atomic_fetch_add(&self->ref_count, 1);
+
+    return self;
+}
 
 void
-release_dict(Dictionary* self);
+release_dict(Dictionary* self) {
+    if (!self) return;
+
+    const size_t old_ref_count = atomic_fetch_sub_explicit(
+        &self->ref_count,
+        1,
+        memory_order_relaxed
+    );
+
+    if (old_ref_count == 1) {
+        atomic_thread_fence(memory_order_acquire);
+
+        free(self->array[0].elements);
+        if (self->array[1].elements) free(self->array[1].elements);
+
+        free(self);
+    }
+}
 
 any
 dict_get(
     Dictionary* self,
     let_any key
-);
+) {
+    if (!self) return NULL;
+
+    if (self->is_rehashing && self->array[1].elements && self->array[1].num_elements != 0) {
+
+        size_t index = self->hash(key) % self->array[1].size;
+        const size_t start_idx = index;
+
+        do {
+            const struct DictElement element = self->array[1].elements[index];
+
+            if (element.state == EMPTY) return NULL;
+
+            if (element.state == USED && self->compare(key, element.key) == 0)
+                return element.data;
+
+            index = (index + 1) % self->array[1].size;
+        } while (index != start_idx);
+    }
+}
 
 bool
 dict_set(
